0c1b155bff8cee8738c4b51f27c792d36ebca7c2
rensphilipsen <philipsen.rens@gmail.com>
2018-04-19 16:53:24 +0200

    Fix some styling

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <h5 class="pb-2 mb-0">Order tracker</h5>
 
     <div class="my-3 p-3 bg-white rounded box-shadow">
-        <h6 class="border-bottom border-gray pb-2 mb-0">Order Tracker</h6>
 
         @if (session('message'))
             <div class="alert alert-success">
